CarbonROM/android_packages_apps_Settings

panels: Add Bluetooth panel
Signed-off-by: Joe Maples <joe@maples.dev>
Change-Id: I5136d35435a4d12d55f84ceaa3ef0f88edcdb78f

src/com/android/settings/panel/PanelFeatureProviderImpl.java
@@ -40,6 +40,8 @@ public class PanelFeatureProviderImpl implements PanelFeatureProvider {
                 return WifiPanel.create(context);
             case Settings.Panel.ACTION_VOLUME:
                 return VolumePanel.create(context);
+            case Settings.Panel.ACTION_BLUETOOH:
+                return BluetoothPanel.create(context);
         }
 
         throw new IllegalStateException("No matching panel for: "  + panelType);

f1r3w411/platform_packages_apps_Settings

src/com/android/settings/panel/PanelFeatureProviderImpl.java
@@ -42,6 +42,8 @@ public class PanelFeatureProviderImpl implements PanelFeatureProvider {
                 return VolumePanel.create(context);
             case Settings.Panel.ACTION_MOBILE_DATA:
                 return MobileDataPanel.create(context);
+            case Settings.Panel.ACTION_BLUETOOTH:
+                return BluetoothPanel.create(context);
         }
 
         throw new IllegalStateException("No matching panel for: "  + panelType);